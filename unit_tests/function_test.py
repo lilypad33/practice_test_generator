fd5f9387cbb1757173d687b8f7d717c577067615
Lilypad <lilypad64123@gmail.com>
2025-05-28 19:12:58 -0500

    Tweaked the tests so that it has cross-OS compatibiltiy (escape characters, opps)

--- unit_tests/function_test.py
+++ unit_tests/function_test.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from unittest.mock import patch
 from test_generator import list_csv_files
 from test_generator import select_option
@@ -8,7 +9,7 @@ def test_list_csvs_as_list():
     Checks that the list_csv_files function returns a list
     '''
 
-    example_directory = "practice_tests\\AWS"
+    example_directory = os.path.join("practice_tests", "AWS")
 
     directory_list = list_csv_files(example_directory)
 
@@ -19,9 +20,12 @@ def test_select_option_valid():
     '''
     Ensures the select option function returns a number, which select_option converts from a string into an integer.
     '''
-    test_directory = list_csv_files("practice_tests\\AWS")
+    example_directory = os.path.join("practice_tests", "AWS")
+
+    test_list = list_csv_files(example_directory)
+
 
     with patch("builtins.input", return_value = "1"):
-        test_value = select_option(test_directory)
+        test_value = select_option(test_list)
         assert isinstance(test_value, int)
 
